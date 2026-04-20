b85818d16b4d59db91a7ccfb37e56a51446f31d9
Sara <sarawaynua@gmail.com>
2026-04-20 08:26:16 -0700

    Added my new feature

--- project/e-shop/e-shop-frontend/src/components/AdminLayout.tsx
+++ project/e-shop/e-shop-frontend/src/components/AdminLayout.tsx
@@ -3,6 +3,7 @@ import { Sidebar } from "./admin/sidebar";
 import { OverviewPage } from "./admin/overview";
 import { ProductsPage } from "./admin/ProductPage";
 import { OrdersPage } from "./admin/OrdersPage";
+import { GuestsPage } from "./admin/GuestsPage";
 
 const AdminLayout: React.FC = () => {
   const [activeItem, setActiveItem] = useState("overview");
@@ -16,7 +17,7 @@ const AdminLayout: React.FC = () => {
       case "orders":
         return <OrdersPage />;
       case "guests":
-        return <div className="p-8 text-white text-2xl">Guests Page</div>;
+        return <GuestsPage/>;
       case "analytics":
         return <div className="p-8 text-white text-2xl">Analytics Page</div>;
       default:
